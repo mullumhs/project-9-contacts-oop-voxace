39e51444658354247d567d52623cfbd79a9a0119
David Steedman <dave@voxace.com>
2024-05-14 02:37:29 +0000

    start lab 3

--- lab3.py
+++ lab3.py
@@ -13,3 +13,15 @@
 # 4. Reproduce the instances of the Contact class that you created in Lab 2
 # 5. Call your new instance method on one Contact and print the result
 # 6. Use the class method to print out the total number of contacts
+
+class Contact:
+    number_of_contacts = 0
+    
+    def __init__(self, name, phone_number, email):
+        self.name = name
+        self.phone_number = phone_number
+        self.email = email
+        Contact.number_of_contacts += 1
+
+c1 = Contact("John Doe", "E123", "john@gmail.com")
+c2 = Contact("Jane Smith", "E456", "jane@gmail.com")
